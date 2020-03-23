f904e5b75e5e91673944d137ed0d5e2fe14a86df
nickmurr <nickmur72@gmail.com>
2020-03-23 21:39:54 +0200

    test_cases solution

--- python/SquareEveryDigit/main.py
+++ python/SquareEveryDigit/main.py
@@ -4,3 +4,13 @@ def square_digits(num):
 
 x = square_digits(9119)
 print(x)
+
+
+###
+def square_digits_test(number):
+    new_number = ""
+    for digit in str(number):
+        digit = int(digit) ** 2
+        new_number += str(digit)
+    return int(new_number)
+###
