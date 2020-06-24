d2a0a6f7973fad2dd45cfeb7c6ac1ffc569d84e4
nickmurr <nickmur72@gmail.com>
2020-06-24 21:41:22 +0300

    devisible

--- rust/devisible/src/main.rs
+++ rust/devisible/src/main.rs
@@ -23,4 +23,20 @@ mod tests {
         assert_eq!(is_divisible(17, 17, 17), true);
         assert_eq!(is_divisible(17, 1, 17), true);
     }
+
+    fn sol(n: i32, x: i32, y: i32) -> bool {
+        n % x == 0 && n % y == 0
+    }
+
+    #[test]
+    fn random_tests() {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+        for _ in 0..100 {
+            let n = rng.gen_range(1, 101);
+            let x = rng.gen_range(1, 6);
+            let y = rng.gen_range(1, 11);
+            assert_eq!(is_divisible(n, x, y), sol(n, x, y));
+        }
+    }
 }
